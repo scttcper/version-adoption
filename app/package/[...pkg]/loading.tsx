9955178a612436672e49ee7296a1b6423f97b6f5
Scott Cooper <scttcper@gmail.com>
2023-11-09 11:30:40 -0800

    whatever

--- app/package/[...pkg]/loading.tsx
+++ app/package/[...pkg]/loading.tsx
@@ -5,7 +5,7 @@ import { Spinner } from '../../../components/Spinner';
 export default function Layout() {
   return (
     <div>
-      <div className="mb-5 mt-8">
+      <div className="mb-8 mt-8">
         <Link href="/" className="text-gray-700 font-semibold">
           Home
         </Link>
