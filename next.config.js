5fb43b417b3f1c91fefd2b13d10fb98bae0d90c6
Scott Cooper <scttcper@gmail.com>
2023-11-13 13:47:58 -0800

    Revert "enable ppr"
    This reverts commit 781222d12358e93bd8cdc06c3f71e0f214369b01.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@
  **/
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    ppr: true,
-  },
   async headers() {
     return [
       {
@@ -41,9 +38,10 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
+
 // Injected content via Sentry wizard below
 
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(
   module.exports,
@@ -53,8 +51,8 @@ module.exports = withSentryConfig(
 
     // Suppresses source map uploading logs during build
     silent: true,
-    org: 'scooper',
-    project: 'version-adoption',
+    org: "scooper",
+    project: "version-adoption",
   },
   {
     // For all available options, see:
@@ -67,7 +65,7 @@ module.exports = withSentryConfig(
     transpileClientSDK: true,
 
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
+    tunnelRoute: "/monitoring",
 
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
