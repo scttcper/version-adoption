240ebe4f4e30bddb82dd9fc09dcd1efb86d0e59b
Scott Cooper <scttcper@gmail.com>
2023-11-23 10:44:46 -0800

    mobile padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import '../styles/index.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="px-4 md:px-1">{children}</body>
     </html>
   );
 }
