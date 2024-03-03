67bb81ea85d05cc8165903e49879e980f7b431a0
Scott Cooper <scttcper@gmail.com>
2024-03-03 15:14:47 -0800

    update packages

--- components/VersionTable.tsx
+++ components/VersionTable.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { Cell, Column, Row, Table, TableBody, TableHeader } from 'react-aria-components';
-import type { ColumnProps } from 'react-aria-components';
 
 import { VersionData } from '../interfaces/types';
 
@@ -9,10 +8,6 @@ type Props = {
   data: VersionData;
 };
 
-function MyColumn<T extends object>(props: ColumnProps<T>) {
-  return <Column {...props}>{() => <>{props.children}</>}</Column>;
-}
-
 function VersionTable({ data, total }: Props) {
   return (
     <Table aria-label="Version data" className="mb-5 tabular-nums">
@@ -20,14 +15,14 @@ function VersionTable({ data, total }: Props) {
         columns={[{ key: 'version', isRowHeader: true }, { key: 'downloads' }, { key: '% total' }]}
       >
         {(column) => (
-          <MyColumn isRowHeader={column.isRowHeader} className="py-2 px-3">
+          <Column id={column.key} isRowHeader={column.isRowHeader} className="py-2 px-3">
             {column.key}
-          </MyColumn>
+          </Column>
         )}
       </TableHeader>
       <TableBody items={data}>
         {([version, downloads]) => (
-          <Row key={version}>
+          <Row id={version} key={version}>
             <Cell className="py-1 px-3 text-center border-t border-gray-300">{version}</Cell>
             <Cell className="py-1 px-3 text-right border-t border-gray-300">
               {downloads.toLocaleString()}
